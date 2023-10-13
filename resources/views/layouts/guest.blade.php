18afa00c180a4ec4d901eca41de8f2f13a280d20
bouyjdadi <boujdadikaouthar@gmail.com>
2023-10-13 14:30:48 +0100

    add login page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,16 +15,23 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
-
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
+        <div class="min-h-screen pt-6 sm:pt-0 ">
+           <div class="absolute inset-0 flex items-center justify-center" style="background-image: url('/images/bg-login.svg');" aria-label="Image de fond">
+                <div class="w-[45%] login-div px-12 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+                    {{ $slot }}
+                </div>
+                <div class="footer-login text-xs">
+                    <div class="absolute bottom-4 left-4 partie-droite">
+                        <p class="text-white">Faistroquer.fr © 2023. Design by SEOMANIAK</p>
+                    </div>
+                    <div class="absolute bottom-4 right-4 partie-gauche">
+                        <p class="text-white">Politique de confidentialité |
+                            {{ __('Conditions générales d\'utilisation') }}
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
+
     </body>
 </html>
