f33d9c9f27ca6e439bd0a6ce0faaf648fe180f1a
oussamaseomaniak <oussamaseomaniak@gmail.com>
2024-03-15 13:35:51 +0000

    changesslick

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
 
-        @vite(['resources/css/app.css', 'resources/js/app.js','resources/carousel/slick/slick.css','resources/carousel/slick/slick-theme.css','resources/carousel/slick/slick.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js','resources/carousel/slick/slick.css','resources/carousel/slick/slick-theme.css'])
         
 
         <!-- Main Styles -->
@@ -73,9 +73,9 @@
         
     });
 </script>
-<!-- For Production 
+
 <script type="text/javascript" src="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.min.js"></script>
--->
+
 
     </body>
 </html>
