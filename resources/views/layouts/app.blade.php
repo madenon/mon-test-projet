031456c7b08d6f95772357c16a6f5075d2da325d
oussamaseomaniak <oussamaseomaniak@gmail.com>
2024-08-08 12:04:11 +0100

    add slickjs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,6 +66,7 @@
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
         <script src="{{ asset('js/app.js') }}"></script>
+        <script type="text/javascript" src="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.min.js"></script>
         <script>
     $(document).ready(function () {
         // Set CSRF token for all AJAX requests
